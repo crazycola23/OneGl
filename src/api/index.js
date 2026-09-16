6da4b989c572dc9235c2a79978303770bb445e53
crazycola23 <165279137+crazycola23@users.noreply.github.com>
2026-09-16 15:09:10 +0800

    refactor(api): export native API helpers

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,4 @@
-export { apiKeyAuth } from "./auth.js";
+export { apiCredentialFromHeaders, isApiRequestAuthorized, secureStringEqual } from "./auth.js";
+export { parseBatchCreate, parseKeywordsCreate, parseLimit, parseProjectCreate } from "./contracts.js";
+export { ApiHttpError, errorPayload, readJsonBody, sendJson } from "./http.js";
 export { openApiDocument } from "./openapi.js";
